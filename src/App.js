6d0465977a18d882214c73929970d0a2b6cfed1f
liwuid <alexiades.v@gmail.com>
2025-09-17 14:12:14 -0400

    css and js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React RIGHT NOWWWW
-        </a>
-      </header>
-    </div>
-  );
+    const [topfivefilms, setFilms] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/topfivefilms")
+            .then((response) => response.json())
+            .then((data) => {
+                const films = data.map(f => ({
+                    id: f[0],
+                    title: f[1],
+                }));
+                setFilms(films);
+            })
+            .catch((error) => console.error("Error displaying films:", error));
+    }, []);
+
+    return (
+        <div className="Sakila-DB">
+            <h1>Top Five Films</h1>
+            <div className="top-five-films">
+                {topfivefilms.map((film) => (
+                    <div key={film.id} className="film-item">
+                        <a href={`/film/${film.id}`}>
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/2503/2503508.png"
+                            alt={film.title}
+                            className="film-image"
+                        />
+                        </a>
+                        <p className="film-title">{film.title}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
