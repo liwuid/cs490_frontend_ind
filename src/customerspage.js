adf76d72bfb4262a897b931e8db4f1eef9ff223e
liwuid <alexiades.v@gmail.com>
2025-09-26 01:16:07 -0400

    customer

--- src/customerspage.js
+++ src/customerspage.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react"
 import "./App.css"
 
 function CustomersPage() {
-    const [customer, setCustomer] = useState([]);
+    const [customers, setCustomers] = useState([]);
     const [pagination, setPagination] = useState(null);
 
     useEffect(() => {
         const fetchCustomers = async (page=1) => {
             try {
-                const response = fetch(`https://localhost:5000/customers?page=${page}`);
-                const data = (await response).json();
+                const response = await fetch(`http://localhost:5000/customers?page=${page}`);
+                const data = await response.json();
 
                 const dataCustomers = data.customers.map((customer) => ({
                     id: customer[0],
@@ -17,14 +17,41 @@ function CustomersPage() {
                     lastname: customer[2],
                 }));
 
-                setCustomer(dataCustomers);
+                setCustomers(dataCustomers);
                 setPagination(data.pagination);
             } catch (error) {
-                console.error("Error fetching custoemrs:", error);
+                console.error("Error fetching customers:", error);
             }
         };
         fetchCustomers(1);
     }, []);
+
+    return (
+        <div className="customer-page">
+            <h1>Customers</h1>
+            <div className="customer-table-wrap">
+                <table className="customer-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {customers.map((customer) => (
+                            <tr key={customer.id}>
+                                <td>{customer.id}</td>
+                                <td>{customer.firstname}</td>
+                                <td>{customer.lastname}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 };
 
 export default CustomersPage;
