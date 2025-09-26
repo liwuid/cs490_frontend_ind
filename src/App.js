08688d785b3be9d7ccdbb6820bb0ea4d2378aa2f
liwuid <alexiades.v@gmail.com>
2025-09-26 03:51:20 -0400

    pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import FilmDetails from "./filmdetails";
 import ActorDetails from "./actordetails";
 import LandingPage from "./landingpage";
@@ -6,24 +7,28 @@ import FilmsPage from "./filmspage";
 import CustomersPage from "./customerspage";
 import './App.css';
 
+const theme = createTheme();
+
 function App() {
     return (
-        <Router>
-            <div>
-                <nav className="navbar">
-                    <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/films/search" className="nav-link">Films</Link>
-                    <Link to="/customers" className="nav-link">Customers</Link>
-                </nav>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/film/:id" element={<FilmDetails />} />
-                <Route path="/actor/:id" element={<ActorDetails />} />
-                <Route path="/films/search" element={<FilmsPage />} />
-                <Route path="/customers" element={<CustomersPage />} />
-            </Routes>
-            </div>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div>
+                    <nav className="navbar">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/films/search" className="nav-link">Films</Link>
+                        <Link to="/customers" className="nav-link">Customers</Link>
+                    </nav>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/film/:id" element={<FilmDetails />} />
+                    <Route path="/actor/:id" element={<ActorDetails />} />
+                    <Route path="/films/search" element={<FilmsPage />} />
+                    <Route path="/customers" element={<CustomersPage />} />
+                </Routes>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
 
--- src/customerspage.js
+++ src/customerspage.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from "react"
-import "./App.css"
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import "./App.css";
 
 function CustomersPage() {
     const [customers, setCustomers] = useState([]);
-    const [pagination, setPagination] = useState(null);
+    const [pagination, setPagination] = useState({page: 1, pages: 1});
 
+    const fetchCustomers = async (page=1) => {
+        try {
+            const response = await fetch(`http://localhost:5000/customers?page=${page}`);
+            const data = await response.json();
+
+            const dataCustomers = data.customers.map((customer) => ({
+                id: customer[0],
+                firstname: customer[1],
+                lastname: customer[2],
+            }));
+
+            setCustomers(dataCustomers);
+            setPagination(data.pagination);
+        } catch (error) {
+            console.error("Error fetching customers:", error);
+        }
+    };
     useEffect(() => {
-        const fetchCustomers = async (page=1) => {
-            try {
-                const response = await fetch(`http://localhost:5000/customers?page=${page}`);
-                const data = await response.json();
-
-                const dataCustomers = data.customers.map((customer) => ({
-                    id: customer[0],
-                    firstname: customer[1],
-                    lastname: customer[2],
-                }));
-
-                setCustomers(dataCustomers);
-                setPagination(data.pagination);
-            } catch (error) {
-                console.error("Error fetching customers:", error);
-            }
-        };
         fetchCustomers(1);
     }, []);
+
+    const pageChange = (event, value) => {
+        fetchCustomers(value);
+    };
 
     return (
         <div className="customer-page">
@@ -50,8 +56,16 @@ function CustomersPage() {
                     </tbody>
                 </table>
             </div>
+            <Stack spacing={2} style={{ marginTop: "20px", alignItems: "center"}}>
+                <Pagination
+                    count={pagination.pages}
+                    page={pagination.page}
+                    onChange={pageChange}
+                    color="primary"
+                />
+            </Stack>
         </div>
-    )
-};
+    );
+}
 
 export default CustomersPage;
